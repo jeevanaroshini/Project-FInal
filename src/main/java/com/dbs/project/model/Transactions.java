9821b1de6f9e904d2a7c0a6eba1de3c23afbc475
Shariq <shariq.mohammed01@gmail.com>
2022-09-24 16:58:53 +0000

    Added by Shariq

--- src/main/java/com/dbs/project/model/Transactions.java
+++ src/main/java/com/dbs/project/model/Transactions.java
@@ -1,112 +1,97 @@
 package com.dbs.project.model;
 
-import java.sql.Timestamp;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Entity
 public class Transactions {
 
-	int id;
-	long amount;
-	int cusId;
+
+	@Id
+	String cusId;
 	String cusName;
-	int recId;
+	long amount;
+	String recId;
 	String recName;
-	int recBankId;
-	String recBankName;
-	String msgCode;
-	String transferType;
-	Timestamp timestamp;
+	String timestamp;
+	boolean status;
+	
 	public Transactions() {
-		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Transactions(int id, long amount, int cusId, String cusName, int recId, String recName, int recBankId,
-			String recBankName, String msgCode, String transferType, Timestamp timestamp) {
+
+	public Transactions(String cusId, String cusName, long amount, String recId, String recName,
+			String timestamp,boolean status) {
 		super();
-		this.id = id;
-		this.amount = amount;
+
 		this.cusId = cusId;
 		this.cusName = cusName;
+		this.amount = amount;
 		this.recId = recId;
 		this.recName = recName;
-		this.recBankId = recBankId;
-		this.recBankName = recBankName;
-		this.msgCode = msgCode;
-		this.transferType = transferType;
 		this.timestamp = timestamp;
+		this.status =status;
 	}
-	public int getId() {
-		return id;
+
+	
+
+	public boolean isStatus() {
+		return status;
 	}
-	public void setId(int id) {
-		this.id = id;
+
+	public void setStatus(boolean status) {
+		this.status = status;
 	}
-	public long getAmount() {
-		return amount;
-	}
-	public void setAmount(long amount) {
-		this.amount = amount;
-	}
-	public int getCusId() {
+
+	public String getCusId() {
 		return cusId;
 	}
-	public void setCusId(int cusId) {
+
+	public void setCusId(String cusId) {
 		this.cusId = cusId;
 	}
+
 	public String getCusName() {
 		return cusName;
 	}
+
 	public void setCusName(String cusName) {
 		this.cusName = cusName;
 	}
-	public int getRecId() {
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(long amount) {
+		this.amount = amount;
+	}
+
+	public String getRecId() {
 		return recId;
 	}
-	public void setRecId(int recId) {
+
+	public void setRecId(String recId) {
 		this.recId = recId;
 	}
+
 	public String getRecName() {
 		return recName;
 	}
+
 	public void setRecName(String recName) {
 		this.recName = recName;
 	}
-	public int getRecBankId() {
-		return recBankId;
-	}
-	public void setRecBankId(int recBankId) {
-		this.recBankId = recBankId;
-	}
-	public String getRecBankName() {
-		return recBankName;
-	}
-	public void setRecBankName(String recBankName) {
-		this.recBankName = recBankName;
-	}
-	public String getMsgCode() {
-		return msgCode;
-	}
-	public void setMsgCode(String msgCode) {
-		this.msgCode = msgCode;
-	}
-	public String getTransferType() {
-		return transferType;
-	}
-	public void setTransferType(String transferType) {
-		this.transferType = transferType;
-	}
-	public Timestamp getTimestamp() {
+
+	public String getTimestamp() {
 		return timestamp;
 	}
-	public void setTimestamp(Timestamp timestamp) {
+
+	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
 	}
-	@Override
-	public String toString() {
-		return "Transactions [id=" + id + ", amount=" + amount + ", cusId=" + cusId + ", cusName=" + cusName
-				+ ", recId=" + recId + ", recName=" + recName + ", recBankId=" + recBankId + ", recBankName="
-				+ recBankName + ", msgCode=" + msgCode + ", transferType=" + transferType + ", timestamp=" + timestamp
-				+ "]";
-	}
+	
+	
 	
 	
 }
